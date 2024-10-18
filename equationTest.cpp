bd203d0f2b1f069d046b02afd2736d9f4e541b83
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-10-18 12:20:15 +0500

    Update equationTest.cpp

--- equationTest.cpp
+++ equationTest.cpp
@@ -25,15 +25,17 @@ CPPUNIT_TEST_SUITE_REGISTRATION(Test);
 
 int main()
 {
-  CPPUNIT_NS::TestResult result;
+  CPPUNIT_NS::TestResult controller;
+
+  CPPUNIT_NS::TestResultCollector result;
   controller.addListener(&result);
 
-CPPUNIT_NS::BriefTestProgressListener progress;
-controller.addListener(&progress);
+  CPPUNIT_NS::BriefTestProgressListener progress;
+  controller.addListener(&progress);
 
-CPPUNIT_NS::TestRunner runner;
-runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
-runner.run(controller);
+  CPPUNIT_NS::TestRunner runner;
+  runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+  runner.run(controller);
 
-return result.wasSuccesfull() ? 0 : 1;
+  return result.wasSuccesfull() ? 0 : 1;
 }
