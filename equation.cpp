7896a153d4c6d0a24949837ac0fa7a4f9155648d
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-10-18 14:24:15 +0500

    Update equation.cpp

--- equation.cpp
+++ equation.cpp
@@ -6,23 +6,29 @@ const double e = 1E-07;
 int main(int ac, char **av)
 {
     std::cout << "Solution of a quadratic equation" << std::endl;
-    double a, b, c, d, x;
+    double a, b, c;
 
     std::cout << "Enter the coefficient of the quadratic equation:" << std::endl;
     std::cin >> a;
     std::cin >> b;
     std::cin >> c;
 
+    calc(a, b, c);
+}
+
+void calc(double a, double b, double c)
+{
     if(abs(a) < e)
         std::cout << "The equation is not quadratic." << std::endl;
     else
     {
-        d = b * b - 4.0 * a * c;
+        double d = b * b - 4.0 * a * c;
         std::cout << "d = " << d << std::endl;
         if(d < 0.0)
             std::cout << "The equation has no real roots." << std::endl;
         else
         {
+            double x;
             if(abs(d) < e)
             {
                 std::cout << "The equation has one real roots:" << std::endl;
