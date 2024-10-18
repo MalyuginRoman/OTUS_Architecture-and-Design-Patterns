9adefe1c19c5c52db4e06cbc9a4b52c4856126d5
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-10-18 15:00:37 +0500

    Update equationTest.cpp

--- equationTest.cpp
+++ equationTest.cpp
@@ -1,70 +1,51 @@
 #include <iostream>
-#include <sstream>
-#include <cstring>
-#include <string>
-#include <ctime>
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/extensions/HelperMacros.h>
 
-class TestSolution { public:
-    #define CHECK(a,b)  check<long long>(a, b, #a, #b, __FILE__, __LINE__, __FUNCTION__);
-    #define CHECKT(a)   check<bool>(a, true, #a, "true", __FILE__, __LINE__, __FUNCTION__);
-    #define CHECKS(a,b) check<cs>(a, b, #a, #b, __FILE__, __LINE__, __FUNCTION__);
-    typedef const std::string& cs;
-    int checks, fails; std::ostringstream serr; std::istringstream *in;
-    Cppunit() { checks = fails = 0;}
-    void test_cin(cs s){ in = new std::istringstream(s); std::cin.rdbuf(in->rdbuf()); }
-    void fail_hdr(cs stra, cs strb, cs file, int line, cs func) {
-        serr << "==================================================" << std::endl;
-        serr << "FAIL: " << func << std::endl;
-        serr << "--------------------------------------------------" << std::endl;
-        serr << "File \"" << file << "\", line " << line << " in " << func << std::endl;
-        serr << "  Checking " << stra << " == " << strb << std::endl;
-    }
-    template <typename T> void check(T a, T b, cs stra, cs strb, cs file, int line, cs func) {
-        checks++; if (a == b) { std::cout << "."; return; }
-        fails++; std::cout << "F"; fail_hdr(stra, strb, file, line, func);
-        serr << "  Error: \"" << a << "\" ! = \"" << b << "\"" << std::endl << std::endl;
-    }
-    virtual void single_test() {}
-    virtual void test_list() { single_test(); }
-    double dclock() { return double(clock()) / CLOCKS_PER_SEC; }
-    int status() {
-        std::cout << std::endl; if (fails) std::cout << serr.str();
-        std::cout << "--------------------------------------------------" << std::endl;
-        std::cout << "Ran " << checks << " checks in " << dclock() << "s" << std::endl << std::endl;
-        if (fails) std::cout << "FAILED (failures=" << fails << ")"; else std::cout << "OK" << std::endl;
-        return fails > 0;
-    }
-    int run() { std::streambuf* ocin = std::cin.rdbuf(); test_list(); std::cin.rdbuf(ocin); return status(); }
+class TestSolution : public CPPUNIT_NS::TestCase
+{
+  CPPUNIT_TEST_SUITE(TestSolution);
+  CPPUNIT_TEST(calculate);
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+  void setUp(void) {}
+  void tearDown(void){}
+
+protected:
+  void calculate();
+//  void noRoot();
+//  void oneRoot();
+//  void twoRoot();
+//  void firstCoef();
+//  void inPutCorrect();
 };
 
-// Class under test.
-class test_class {
-    public: int calculate(){
-        int n, m;
-        std::cin >> n >> m;
-        return n + m;
-    }
-};
+CPPUNIT_TEST_SUITE_REGISTRATION(TestSolution);
 
-// Test examples.
-class Cppunit_tests: public Cppunit {
-    void single_test() {
-        // Integral type match check.
-        CHECK(2 + 2, 4);
+void TestSolution::calculate()
+{
+  CPPUNIT_ASSERT_EQUAL();
+}
 
-        // Boolean type value check.
-        CHECKT(2 + 2 == 4);
 
-        // String match check.
-        CHECKS("a" "b", "ab");
+int main()
+{
+  CPPUNIT_NS::TestResult controller;
 
-        // Stdin override example.
-        test_cin("2\n2");
-        CHECK((new test_class)->calculate(), 4);
-    }
-};
+  CPPUNIT_NS::TestResultCollector result;
+  controller.addListener(&result);
 
-int main(int argc, char *argv[]) {
-    // Test invocation example.
-    return (new Cppunit_tests)->run();
+  CPPUNIT_NS::BriefTestProgressListener progress;
+  controller.addListener(&progress);
+
+  CPPUNIT_NS::TestRunner runner;
+  runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+  runner.run(controller);
+
+  return result.wasSuccessful() ? 0 : 1;
 }
