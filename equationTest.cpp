a4e0172dc9166a7b8c072eed4f1c28ae1dc651a5
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-10-18 11:57:48 +0500

    Create equationTest.cpp

---
+++ equationTest.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cppunit/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/extensions/HelpMacros.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+
+class TestSolution : public CPPUNIT_NS::TestCase
+{
+  CPPUNIT_TEST_SUITE(Test);
+  CPPUNIT_TEST(testEquationSolution);
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+  void setUp(void) {}
+  void tearDown(void){}
+
+protected:
+  void testHelloWord(void) {
+    system(0, 0 ,0)
+  }
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(Test);
+
+int main()
+{
+  CPPUNIT_NS::TestResult controller;
+  controller.addListener(&result);
+
+CPPUNIT_NS::BriefTestProgressListener progress;
+controller.addListener(&progress);
+
+CPPUNIT_NS::TestRunner runner;
+runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+runner.run(controller);
+
+return result.wasSuccesfull() ? 0 : 1;
+}
