8c19726f3c3d0ffea623140e6b7eb7c01607e396
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-10-18 16:17:55 +0500

    Update equation.cpp

--- equation.cpp
+++ equation.cpp
@@ -1,20 +1,6 @@
 #include <iostream>
 #include "math.h"
-
-const double e = 1E-07;
-
-double discr(double a, double b, double c)
-{
-    return b * b - 4.0 * a * c;
-}
-
-int root(double a, double b, double d, bool i)
-{
-    if(i == false)
-        return (-b + sqrt(d) / (2 * a));
-    else
-        return (-b - sqrt(d) / (2 * a));
-}
+#include "config.h'
 
 int main(int ac, char **av)
 {
