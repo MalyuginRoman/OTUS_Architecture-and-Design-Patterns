436d5b639c15ec9c4a73b073f21fedfc46902861
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-11-28 13:56:54 +0500

    Update CommandRotate.cpp

--- CommandRotate.cpp
+++ CommandRotate.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 #include "CommandRotate.h"
 
-CommandRotate::CommandRotate()
+class CommandRotateP
+{
+public:
+    object* obj;
+
+    CommandRotateP(object* obj) :
+        obj(obj)
+    {
+    }
+};
+
+CommandRotate::CommandRotate(object* obj) :
+    imp(new CommandRotateP(obj))
 {
     this->getStr("CommandRotate");
 }
 
+CommandRotate::~CommandRotate() { delete imp;}
+
 void CommandRotate::execute()
 {
-    //std::cout << "Start execute of CommandRotate" << std::endl;
-    throw std::runtime_error("CommandRotate");
+    std::cout << "Start execute of CommandRotate" << std::endl;
+    if(imp->obj->place().placeX < 0 && imp->obj->place().placeY < 0)
+        throw std::runtime_error ("Object not found");
+    if(!imp->obj->getAngularVelocity(imp->obj, 0))
+        throw std::runtime_error ("Unknown angular velocity");
+    std::cout << imp->obj->place().angular << std::endl;
+    if(!imp->obj->getAngular(imp->obj, dt))
+        throw std::runtime_error ("Unknown position");
+    std::cout << imp->obj->place().angular << std::endl;
 }
 
 void CommandRotate::getStr(std::string value)
@@ -20,4 +41,9 @@ void CommandRotate::getStr(std::string value)
 std::string CommandRotate::setStr()
 {
     return this->str;
+}
+
+object* CommandRotate::obj() const
+{
+    return imp->obj;
 }
