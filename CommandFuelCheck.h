f7335c8e8bc9c86d35bd16d96f3835dfd66d0421
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-11-28 12:27:06 +0500

    Create CommandFuelCheck.h

---
+++ CommandFuelCheck.h
@@ -0,0 +1,23 @@
+#ifndef COMMANDFUELCHECK_H
+#define COMMANDFUELCHECK_H
+#include "ICommand.h"
+#include "object.h"
+#include <string>
+
+class CommandFuelCheck : public ICommand
+{
+public:
+    CommandFuelCheck(object* obj);
+    ~CommandFuelCheck();
+
+    object* obj() const;
+
+    void execute();
+    void getStr(std::string value);
+    std::string setStr();
+private:
+    std::string str;
+    class CommandFuelCheckP* imp;
+};
+
+#endif // COMMANDFUELCHECK_H
