c0f5de9a5bc1c4c2d8eb68a298f942253f6a1561
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-11-28 13:56:24 +0500

    Update CommandMove.cpp

--- CommandMove.cpp
+++ CommandMove.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 #include "CommandMove.h"
 
-CommandMove::CommandMove()
+class CommandMoveP
+{
+public:
+    object* obj;
+
+    CommandMoveP(object* obj) :
+        obj(obj)
+    {
+    }
+};
+
+CommandMove::CommandMove(object* obj) :
+    imp(new CommandMoveP(obj))
 {
     this->getStr("CommandMove");
 }
 
+CommandMove::~CommandMove() { delete imp;}
+
 void CommandMove::execute()
 {
-    //std::cout << "Start execute of CommandMove" << std::endl;
-    throw std::runtime_error ("CommandMove");
+    std::cout << "Start execute of CommandMove" << std::endl;
+    if(imp->obj->place().placeX < 0 && imp->obj->place().placeY < 0)
+        throw std::runtime_error ("Object not found");
+    if(!imp->obj->getVelocity(imp->obj, 0))
+        throw std::runtime_error ("Unknown velocity");
+    std::cout << imp->obj->place().placeX << "," << imp->obj->place().placeY << std::endl;
+    if(!imp->obj->getPosition(imp->obj, dt))
+        throw std::runtime_error ("Unknown position");
+    std::cout << imp->obj->place().placeX << "," << imp->obj->place().placeY << std::endl;
 }
 
 void CommandMove::getStr(std::string value)
@@ -20,4 +41,9 @@ void CommandMove::getStr(std::string value)
 std::string CommandMove::setStr()
 {
     return this->str;
+}
+
+object* CommandMove::obj() const
+{
+    return imp->obj;
 }
