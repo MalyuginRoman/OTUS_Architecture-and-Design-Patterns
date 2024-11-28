4e30a4ec509a305332923bcc06deb7fd8e61f300
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-11-28 13:57:30 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
 
-#include "CommandQueue.h"
-#include "CommandMove.h"
-#include "CommandRotate.h"
-#include "CommandLoger.h"
-#include "CommandRepeat.h"
-#include "ExceptionHandler.h"
+#include "object.h"
+#include "config.h"
 
-int main(int ac, char **av)
+int main(int argc, char *argv[])
 {
+    QCoreApplication a(argc, argv);
+
+    objectVector vector;
+    int count = 1;
+    for(int i = 0; i < count; i++)
+    {
+        int id = i;
+        coord place;
+        react state;
+
+        place.placeX = 0.;
+        place.placeY = 0.;
+        place.angular = 45 * i;
+
+        state.velocity = 100;
+        state.angularVelocity = 20;
+        state.fuel = 10;
+
+        vector.add(id, state, place);
+    }
+
+    std::cout << vector.at(0)->id() << "," << vector.at(0)->state().velocity << "," << vector.at(0)->state().angularVelocity << "," << vector.at(0)->state().fuel
+                   << "," << vector.at(0)->place().placeX << "," << vector.at(0)->place().placeY << "," << vector.at(0)->place().angular << std::endl;
+
     CommandQueue cmd;
-    CommandMove *cmd_move = new CommandMove;
-    CommandRotate *cmd_rotate = new CommandRotate;
-    //CommandLoger *cmd_loger = new CommandLoger;     !п.4,5
-    //CommandRepeat *cmd_repeat = new CommandRepeat;  !п.6,7
+    CommandFuelCheck *cmd_check = new CommandFuelCheck(vector.at(0));
+    CommandMove *cmd_move = new CommandMove(vector.at(0));
+    CommandRotate *cmd_rotate = new CommandRotate(vector.at(0));
+    CommandFuelBurn *cmd_burn = new CommandFuelBurn(vector.at(0));
     std::exception ex;
     ExceptionHandler* handler = new ExceptionHandler(0, ex);
 
+    cmd.add(cmd_check);
     cmd.add(cmd_move);
+    cmd.add(cmd_burn);
     cmd.add(cmd_rotate);
+    cmd.add(cmd_burn);
 
     while(!cmd.isEmpty())
     {
@@ -26,12 +49,10 @@ int main(int ac, char **av)
             cmd.front()->execute();
         } catch( std::exception ex) {
             handler->executeRepeat(handler, &cmd, cmd.front(), ex);
-            //cmd_loger->execute();                   !п.4
-            //cmd.add(cmd_loger);                     !п.5
-            //cmd_repeat->execute(&cmd, cmd.front()); !п.6
-            //cmd.add(cmd_repeat);                    !п.7
         }
-        //cmd.del();                                  !п.4-7
+        cmd.del();
     }
     std::cout << std::endl;
+
+    return a.exec();
 }
