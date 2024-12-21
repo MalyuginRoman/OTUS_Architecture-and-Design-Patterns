87bcc043102d6fe5b99b64707fbe782abbd504ef
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-12-21 14:31:46 +0500

    Create adapter.cpp

---
+++ adapter.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = /*experimental::*/filesystem;
+
+int main()
+{
+  fs::path current_path = fs::current_path();
+  fs::path file_path(current_path;
+
+  cout << file_path.parent_path() << endl;
+  return 0;
+}
