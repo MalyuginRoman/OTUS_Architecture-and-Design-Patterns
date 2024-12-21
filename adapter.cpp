b164852e0ac63024d7a3fe4af6a6c761c4ee0f9e
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-12-21 14:43:56 +0500

    Update adapter.cpp

--- adapter.cpp
+++ adapter.cpp
@@ -9,7 +9,7 @@ namespace fs = filesystem;
 int main()
 {
     fs::path current_path = fs::current_path();
-    fs::path file_path(current_path;
+    fs::path file_path(current_path);
 
     cout << file_path.parent_path() << endl;
     return 0;
