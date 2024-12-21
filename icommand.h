166575e4d6bcf9562bfbc76cc3cb3ff63b52c3b6
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-12-21 13:06:45 +0500

    Create icommand.h

---
+++ icommand.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <list>
+using namespace std;
+
+class ICommand
+{
+public:
+    virtual void execute() = 0;
+};
+
+class MoveCommand : public ICommand
+{
+    void execute()
+    {
+        cout << "Start execute MoveCommand" << endl;
+    }
+};
+
+class RotateCommand : public ICommand
+{
+    void execute()
+    {
+        cout << "Start execute RotateCommand" << endl;
+    }
+};
+
+class CheckCommand : public ICommand
+{
+    void execute()
+    {
+        cout << "Start execute CheckCommand" << endl;
+    }
+};
+
+class BurnCommand : public ICommand
+{
+    void execute()
+    {
+        cout << "Start execute BurnCommand" << endl;
+    }
+};
+
+class MacroCommand : public ICommand
+{
+public:
+    MacroCommand(list<ICommand*> cmds);
+    ~MacroCommand();
+    list<ICommand*> cmds;
+    void execute();
+/*    {
+        cout << "Start execute MacroCommand" << endl;
+    }*/
+private:
+    class MacroCommandP* imp; /**/
+};
