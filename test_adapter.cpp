e67ce16b514acd872dc269819e6dbb8f3d3c2386
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-12-23 14:15:43 +0500

    Update test_adapter.cpp

--- test_adapter.cpp
+++ test_adapter.cpp
@@ -30,6 +30,8 @@ protected:
 
       std::filesystem::path current_path = std::filesystem::current_path();
       std::filesystem::path file_path(current_path);
+      std::list<std::string> list1, list2, list11, list12, list13, list14,
+                             list3, list4, list41, list42, list43, list44;
 
       cout << file_path.parent_path() << endl;
 
@@ -48,10 +50,143 @@ protected:
             }
             else
             {
-                std::string str;
-                while(readFile >> str)
+                if(current_file == "imoving.h")
                 {
-                    std::cout << str << std::endl;
+                    std::string str;
+                    while(readFile >> str)
+                    {
+                        if(str == "virtual")
+                        {
+                            readFile >> str;
+                            list1.push_back(str);
+                            readFile >> str;
+                            list2.push_back(str);
+                            readFile >> str;
+                            readFile >> str;
+                            list11.push_back(str);
+                            readFile >> str;
+                            list12.push_back(str);
+                            readFile >> str;
+                            list13.push_back(str);
+                            readFile >> str;
+                            list14.push_back(str);
+                        }
+                    }
+                    std::string fileWriteName =
+                            p.path().parent_path().generic_string() + "/" +
+                            p.path().filename().replace_extension(".cpp").generic_string();
+                    std::cout << fileWriteName << std::endl;
+                    ofstream f;
+                    f.open(fileWriteName, ios::out);
+                    {
+                        f << "#include \"object.h\"" << std::endl;
+                        f << "class IMoving" << std::endl;
+                        int count = list1.size();
+                        for(int i = 0; i < count; i++)
+                        {
+                            f << list1.front()  << " " << list2.front() << "("
+                              << list11.front() << " " << list12.front();
+                            if(i % 2 == 0)
+                                f << " " << list13.front()  << " " << list14.front();
+                            f << ")" << std::endl;
+                            f << "{" << std::endl;
+                                if(list2.front() == "getPosition")
+                                {
+                                    f << "  IoC.Resolve(\"Spaceship.Operations.IMovable:position.get\", obj);" << std::endl;
+                                    f << "  return 0;" << std::endl;
+                                }
+                                else if(list2.front() == "getVelocity")
+                                {
+                                    f << "  IoC.Resolve(\"Spaceship.Operations.IMovable:velocity.get\", obj);" << std::endl;
+                                    f << "  return 0;" << std::endl;
+                                }
+                                else if(list2.front() == "setPosition")
+                                {
+                                    f << "  IoC.Resolve(\"Spaceship.Operations.IMovable:position.set\", obj," << std::endl;
+                                    f << "  newValue).Execute();" << std::endl;
+                                    f << "  return 0;" << std::endl;
+                                }
+                                else if(list2.front() == "setVelocity")
+                                {
+                                    f << "  return 0;" << std::endl;
+                                }
+                            f << "}" << std::endl;
+                            erase(list1, list2, list11, list12, list13, list14);
+                        }
+                    }
+                    f.close();
+                }
+                else if(current_file == "irotateble.h")
+                {
+                    std::string str;
+                    while(readFile >> str)
+                    {
+                        if(str == "virtual")
+                        {
+                            readFile >> str;
+                            list3.push_back(str);
+                            readFile >> str;
+                            list4.push_back(str);
+                            readFile >> str;
+                            readFile >> str;
+                            list41.push_back(str);
+                            readFile >> str;
+                            list42.push_back(str);
+                            readFile >> str;
+                            list43.push_back(str);
+                            readFile >> str;
+                            list44.push_back(str);
+                        }
+                    }
+                    std::string fileWriteName =
+                            p.path().parent_path().generic_string() + "/" +
+                            p.path().filename().replace_extension(".cpp").generic_string();
+                    std::cout << fileWriteName << std::endl;
+                    ofstream f;
+                    f.open(fileWriteName, ios::out);
+                    {
+                        f << "#include \"object.h\"" << std::endl;
+                        f << "class IRotateble" << std::endl;
+                        int count = list3.size();
+                        for(int i = 0; i < count; i++)
+                        {
+                            f << list3.front()  << " " << list4.front() << "("
+                              << list41.front() << " " << list42.front();
+                            if(i % 2 == 0)
+                                f << " " << list43.front()  << " " << list44.front();
+                            f << ")" << std::endl;
+                            f << "{" << std::endl;
+                                if(list4.front() == "getAngular")
+                                {
+                                    f << "  IoC.Resolve(\"Spaceship.Operations.IRotateble:angular.get\", obj);" << std::endl;
+                                    f << "  return 0;" << std::endl;
+                                }
+                                else if(list4.front() == "getAngularVelocity")
+                                {
+                                    f << "  IoC.Resolve(\"Spaceship.Operations.IRotateble:angular_velocity.get\", obj);" << std::endl;
+                                    f << "  return 0;" << std::endl;
+                                }
+                                else if(list4.front() == "setAngularVelocity")
+                                {
+                                    f << "  IoC.Resolve(\"Spaceship.Operations.IRotateble:angular_velocity.set\", obj," << std::endl;
+                                    f << "  newValue).Execute();" << std::endl;
+                                    f << "  return 0;" << std::endl;
+                                }
+                                else if(list4.front() == "setAngular")
+                                {
+                                    f << "  IoC.Resolve(\"Spaceship.Operations.IRotateble:angular.set\", obj," << std::endl;
+                                    f << "  newValue).Execute();" << std::endl;
+                                    f << "  return 0;" << std::endl;
+                                }
+                            f << "}" << std::endl;
+                            erase2(list3, list4, list41, list42, list43, list44);
+                        }
+                    }
+                    f.close();
+                }
+                else
+                {
+                    std::cout << current_file << " - file not using" << std::endl;
                 }
             }
             readFile.close();
