e74316c5b97879e9339397b6fe2812a21583b3d0
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-12-24 17:05:36 +0500

    Update test_IoC.cpp

--- test_IoC.cpp
+++ test_IoC.cpp
@@ -166,7 +166,7 @@ protected:
                 "Scope2",
                 "MacroCommand1",
                 [&cmd_list1]() { return new MacroCommand(cmd_list1); });
-    std::thread t1{ioc.resolved("MacroCommand1", vector.at(0))->execute}; 
+    //std::thread t1{ioc.resolved("MacroCommand1", vector.at(0))->execute}; 
 
     list<ICommand*> cmd_list2;
     cmd_list2.push_back(cmd_check);
@@ -179,8 +179,8 @@ protected:
 
     //std::thread t2(ioc.resolved("MacroCommand2", vector.at(0), vector.at(1)).execute());
 
-    t1.join();
-    t2.join();
+    //t1.join();
+    //t2.join();
     }
 };
 
