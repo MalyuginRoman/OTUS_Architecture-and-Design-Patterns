783058e38d38dbf7713757ebe9cefd66445a2dda
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-12-25 17:03:46 +0500

    Update equationTest.cpp

--- equationTest.cpp
+++ equationTest.cpp
@@ -6,16 +6,11 @@
 #include <cppunit/BriefTestProgressListener.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/extensions/HelperMacros.h>
-#include "config.h"
 
-class TestSolution : public CPPUNIT_NS::TestCase
+class queue_test : public CPPUNIT_NS::TestCase
 {
-CPPUNIT_TEST_SUITE(TestSolution);
-  CPPUNIT_TEST(testEquationSolution);
-  CPPUNIT_TEST(correctA);
-  CPPUNIT_TEST(noRoot);
-  CPPUNIT_TEST(oneRoot);
-  CPPUNIT_TEST(twoRoot);
+CPPUNIT_TEST_SUITE(queue_test);
+  CPPUNIT_TEST(test1);
 CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -23,38 +18,13 @@ void setUp(void) {}
 void tearDown(void){}
 
 protected:
-  void testEquationSolution(void)
+  void test1(void)
 {
-  root x = solve(1, 2, 1);
-  double result = 1.;
-  CPPUNIT_ASSERT((x.x1 - result) < e);
-}
-  void correctA()
-{
-  double a = 0.;
-  double b = 0.;
-  double c = 0.;
-  CPPUNIT_ASSERT_MESSAGE(std::string("Уравнение не является квадратным"), abs(a) < e);
-  root x = solve(a, b, c);
-}
-  void noRoot()
-{
-  double d = discr(1, 0, 1);
-  CPPUNIT_ASSERT_MESSAGE(std::string("Вещественных корней нет"), d < 0);
-}
-  void oneRoot()
-{
-  double d = discr(1, 2, 1);
-  CPPUNIT_ASSERT_MESSAGE(std::string("Один вещественный корень"), abs(d) < e);
-}
-  void twoRoot()
-{
-  double d = discr(1, 0, -1);
-  CPPUNIT_ASSERT_MESSAGE(std::string("Два вещественных корня"), abs(d) > e);
+  CPPUNIT_ASSERT(1 < 0);
 }
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(TestSolution);
+CPPUNIT_TEST_SUITE_REGISTRATION(queue_test);
 
 int main()
 {
