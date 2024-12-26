8fb82ec5b1e44aca4cfb674584407cf8de35377c
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-12-26 10:24:22 +0500

    Update producer.h

--- producer.h
+++ producer.h
@@ -43,8 +43,12 @@ void test_thread1()
     MoveCommand *cmd_move = new MoveCommand();
     RotateCommand *cmd_rotate = new RotateCommand();
     BurnCommand *cmd_burn = new BurnCommand();
+    EmptyCommand *cmd_empty = new EmptyCommand();
+    HardStopCommand *cmd_hard = new HardStopCommand();
+    SoftStopCommand *cmd_soft = new SoftStopCommand();
 
     bool stop = false;
+    int ic = 0;
 
     queueCmds.push(cmd_check);
     queueCmds.push(cmd_move);
@@ -53,14 +57,48 @@ void test_thread1()
     queueCmds.push(cmd_rotate);
     queueCmds.push(cmd_burn);
     std::thread t1(
-                [&ioc, &queueCmds, &stop](){
+                [&ioc, &queueCmds, &queueCmds_1, &stop, &ic, &cmd_empty, &cmd_hard, &cmd_soft](){
         while(!stop)
         {
             ICommand* cmd = queueCmds.front();;
             cmd->execute();
             queueCmds.pop();
+            if(cmd == cmd_hard)
+            {
+                stop = true;
+                std::cout << "in queue after HardStop : " << std::endl;
+                while(!queueCmds.empty())
+                {
+                    ICommand* cmdh = queueCmds.front();
+                    cmdh->execute();
+                    queueCmds.pop();
+                }
+            }
+            if(cmd == cmd_soft)
+            {
+                stop = true;
+                std::cout << "in queue after SoftStop : " << std::endl;
+                while(!queueCmds.empty())
+                {
+                    ICommand* cmds = queueCmds.front();
+                    queueCmds_1.push(cmds);
+                    queueCmds.pop();
+                }
+                while(!queueCmds_1.empty())
+                {
+                    ICommand* cmds = queueCmds_1.front();
+                    cmds->execute();
+                    queueCmds_1.pop();
+                }
+            }
             if(queueCmds.empty())
                 stop = true;
+            if(ic == 2) queueCmds.push(cmd_empty);
+            if(ic == 3) queueCmds.push(cmd_soft);
+            if(ic == 4) queueCmds.push(cmd_hard);
+            if(ic == 5) queueCmds.push(cmd_empty);
+            if(ic == 6) queueCmds.push(cmd_empty);
+            if(ic == 7) queueCmds.push(cmd_empty);
         }
     });
     t1.join();
