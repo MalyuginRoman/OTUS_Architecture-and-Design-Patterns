310972bd871f09164d3c41b647df597d1a11b2ec
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-01-02 12:43:24 +0500

    Update eventloop.cpp

--- eventloop.cpp
+++ eventloop.cpp
@@ -22,33 +22,43 @@ void eventloop::start(SafeQueue<ICommand *> *cmds, int variant)
 {
     bool stop = false;
     int ic = 0;
+    
     EmptyCommand *cmd_empty = new EmptyCommand();
     HardStopCommand *cmd_hard = new HardStopCommand();
     SoftStopCommand *cmd_soft = new SoftStopCommand();
-    while(!stop)
-    {
-        ic += 1;
-        ICommand* cmd = cmds->front();
-        cmd->execute();
-        cmds->pop();
-        if(cmd == cmd_hard)
-        {
-            stop = true;
-        }
-        if(cmd == cmd_soft)
-        {
-            stop = true;
-            while(!cmds->empty())
+    
+    std::thread t1(
+                [&ioc, &producer, &queueCmds, &handler, &ex](){
+        try {
+            while(!stop)
             {
+                ic += 1;
                 ICommand* cmd = cmds->front();
                 cmd->execute();
                 cmds->pop();
+                if(cmd == cmd_hard)
+                {
+                    stop = true;
+                }
+                if(cmd == cmd_soft)
+                {
+                    stop = true;
+                    while(!cmds->empty())
+                    {
+                        ICommand* cmd = cmds->front();
+                        cmd->execute();
+                        cmds->pop();
+                    }
+                }
+                if(cmds->empty())
+                    stop = true;
+                if(ic == 2) cmds->push(cmd_empty);
+                if(variant == 1 && ic == 3) cmds->push(cmd_soft);
+                if(variant == 2) cmds->push(cmd_hard);
             }
+        } catch( std::exception ex) {
+            handler->executeRepeat(handler, &queueCmds, ex);
         }
-        if(cmds->empty())
-            stop = true;
-        if(ic == 2) cmds->push(cmd_empty);
-        if(variant == 1 && ic == 3) cmds->push(cmd_soft);
-        if(variant == 2) cmds->push(cmd_hard);
-    } /**/
+    });
+    t1.join();
 }
