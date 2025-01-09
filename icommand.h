a853d715722e9e53b16a28c7dbc6af97f6dee015
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-01-09 09:13:59 +0500

    Update icommand.h

--- icommand.h
+++ icommand.h
@@ -57,12 +57,13 @@ template<class T>
 class RegisterCommand : public ICommand
 {
 public:
-    RegisterCommand(map<string, function<T*()>> *m_map, map<string, string> *m_scope);
+    using FuncType = function<T*()>;
+    RegisterCommand(map<string, FuncType> *m_map, map<string, string> *m_scope);
     ~RegisterCommand();
-    map<string, function<T*()>> *m_map;
+    map<string, FuncType> *m_map;
     map<string, string> *m_scope;
     void execute();
-    void registerType(string key_s, string key_f, function<T*()> func);
+    void registerType(string key_s, string key_f, FuncType func);
 private:
     class RegisterCommandP* imp;
 };
