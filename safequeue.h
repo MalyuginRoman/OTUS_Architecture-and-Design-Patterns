9dfbdf272bcc455bdafc9260bfc69c6824852073
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-01-09 17:18:26 +0500

    Create safequeue.h

---
+++ safequeue.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "ICommand.h"
+#include <queue>
+
+class SafeQueue
+{
+public:
+    void add(ICommand* c)
+    {
+        cmd.push(c);
+    };
+    void del()
+    {
+        cmd.pop();
+    };
+    size_t size()
+    {
+        return cmd.size();
+    };
+    bool isEmpty()
+    {
+        return cmd.empty();
+    };
+    ICommand* front()
+    {
+        return cmd.front();
+    };
+private:
+    std::queue<ICommand*> cmd;
+};
