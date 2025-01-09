f202da47d3ded6c5d4203f306fb55529b5d9b688
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-01-09 11:27:34 +0500

    Update ioc.h

--- ioc.h
+++ ioc.h
@@ -5,6 +5,7 @@
 #include <map>
 #include <functional>
 #include <memory>
+#include "icommand.h"
 
 using namespace std;
 
@@ -12,18 +13,8 @@ template<class T>
 class IocContainer
 {
 public:
-    using FuncType = function<T*()>;
-
-    template<class ClassType>
-    void registerType(string key_s, string key_f, FuncType func)
-    {
-        m_scope.emplace(key_s, key_f);
-        m_map.emplace(key_f, func);
-        cout << "Registre " << key_f << " in " << key_s << endl;
-    }
-
     template<typename... Args>
-    shared_ptr<T> resolved(string key_f, Args&&... arg)
+    shared_ptr<T> resolved(string key_f, map<string, function<ICommand*()>> m_map, map<string, string> m_scope, Args&&... arg)
     {
         if (m_map.find(key_f) == m_map.end())
             throw runtime_error ("Unknown key_f");
@@ -34,15 +25,4 @@ public:
         auto func = m_map[key_f];
         return shared_ptr<T>(func());
     }
-
-private:
-    void resolve(string key_f, FuncType type)
-    {
-        m_map.emplace(key_f, type);
-        cout << key_f << " for "<< endl;
-    }
-
-private:
-    map<string, FuncType> m_map;
-    map<string, string> m_scope;
 };
