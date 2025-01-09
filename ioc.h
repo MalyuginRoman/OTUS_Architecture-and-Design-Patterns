45181c36d6ba3f901ef5c46af73796e0808f6172
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-01-09 11:42:09 +0500

    Update ioc.h

--- ioc.h
+++ ioc.h
@@ -14,7 +14,7 @@ class IocContainer
 {
 public:
     template<typename... Args>
-    shared_ptr<T> resolved(string key_f, map<string, function<ICommand*()>> m_map, map<string, string> m_scope, Args&&... arg)
+    shared_ptr<T> resolve(string key_f, map<string, function<ICommand*()>> m_map, map<string, string> m_scope, Args&&... arg)
     {
         if (m_map.find(key_f) == m_map.end())
             throw runtime_error ("Unknown key_f");
