23fef28e6f3ed3447d21f298e28dd6b532046dac
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-01-17 22:20:17 +0500

    Update imoving.h

--- imoving.h
+++ imoving.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "ioc.h"
 
 class object;
 
@@ -9,4 +10,22 @@ public:
     virtual bool setPosition(object *obj) = 0;
     virtual bool getVelocity(object *obj, int du) = 0;
     virtual bool setVelocity(object *obj) = 0;
+};
+
+class MovingAdapter : public IMoving
+{
+    IocContainer<ICommand> ioc;
+    std::map<std::string, std::function<ICommand*()>> m_map;
+    std::map<std::string, std::string> m_scope;
+    int newValue;
+    object* obj;
+public:
+    MovingAdapter(IocContainer<ICommand> ioc,
+                  std::map<std::string, std::function<ICommand*()>> m_map,
+                  std::map<std::string, std::string> m_scope,
+                  object *obj);
+    bool getPosition(object *obj, int dt);
+    bool setPosition(object *obj);
+    bool getVelocity(object *obj, int du);
+    bool setVelocity(object *obj);
 };
