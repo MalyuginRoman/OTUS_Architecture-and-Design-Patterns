5ce29a58f3af0bc142b318dd2d75a118a8673dea
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-01-17 22:21:42 +0500

    Update irotateble.h

--- irotateble.h
+++ irotateble.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "ioc.h"
+
 class object;
 
 class IRotatable
@@ -8,4 +10,22 @@ public:
     virtual bool setAngular(object *obj) = 0;
     virtual bool getAngularVelocity(object *obj, int dv) = 0;
     virtual bool setAngularVelocity(object *obj) = 0;
+};
+
+class RotatingAdapter : public IRotatable
+{
+    IocContainer<ICommand> ioc;
+    std::map<std::string, std::function<ICommand*()>> m_map;
+    std::map<std::string, std::string> m_scope;
+    int newValue;
+    object* obj;
+public:
+    RotatingAdapter(IocContainer<ICommand> ioc,
+                    std::map<std::string, std::function<ICommand*()>> m_map,
+                    std::map<std::string, std::string> m_scope,
+                    object *obj);
+    bool getAngular(object *obj, int dt);
+    bool setAngular(object *obj);
+    bool getAngularVelocity(object *obj, int dv);
+    bool setAngularVelocity(object *obj);
 };
