e6fd12bdf81946fed44ed540bd048d5eb2eb4196
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-01-22 14:49:00 +0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -32,13 +32,13 @@ int main(void)
     const short BUFF_SIZE = 1024;
 
     int erStat;
-
     in_addr ip_to_num;
     inet_pton(AF_INET, SERVER_IP, &ip_to_num);
 
     WSADATA wsData;
     erStat = WSAStartup(MAKEWORD(2,2), &wsData);
 
+#ifdef _WIN32
     if (erStat != 0)
     {
         cout << "Error WinSock version initializaion #";
@@ -47,9 +47,7 @@ int main(void)
     }
     else
         cout << "WinSock initialization is OK" << endl;
-
     SOCKET ClientSock = socket(AF_INET, SOCK_STREAM, 0);
-
     if (ClientSock == INVALID_SOCKET)
     {
         cout << "Error initialization socket # " << WSAGetLastError() << endl;
@@ -60,15 +58,19 @@ int main(void)
         cout << "Client socket initialization is OK" << endl;
 
     sockaddr_in servInfo;
-
     ZeroMemory(&servInfo, sizeof(servInfo));
-
+#else
+    int ServSock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in servInfo;
+    memset(&servInfo, '0', sizeof(servInfo));
+#endif
+    
     servInfo.sin_family = AF_INET;
     servInfo.sin_addr = ip_to_num;
     servInfo.sin_port = htons(SERVER_PORT_NUM);
 
+#ifdef _WIN32
     erStat = connect(ClientSock, (sockaddr*)&servInfo, sizeof(servInfo));
-
     if (erStat != 0)
     {
         cout << "Connection to Server is FAILED. Error # " << WSAGetLastError() << endl;
@@ -78,6 +80,13 @@ int main(void)
     }
     else
         cout << "Connection established SUCCESSFULLY. Ready to send a message to Server" << endl;
+#else
+    if(connect(ClientSock, (struct sockaddr *)&servInfo, sizeof(servInfo))<0)
+    {
+        cout << "Connection failed due to port and ip problems " << endl;
+        return 1;
+    }
+#endif
 
     vector <char> servBuff(BUFF_SIZE), clientBuff(BUFF_SIZE);
     short packet_size = 0;
@@ -104,24 +113,29 @@ int main(void)
         cout << clientBuff.data() << endl;
 //-----------------------------------------------------------------------------------------
 
+#ifdef _WIN32
         if (clientBuff[0] == 'o' && clientBuff[1] == 'u' && clientBuff[2] == 't') {
             shutdown(ClientSock, SD_BOTH);
             closesocket(ClientSock);
             WSACleanup();
             return 0;
         }
+#endif
 
         packet_size = send(ClientSock, clientBuff.data(), clientBuff.size(), 0);
 
+#ifdef _WIN32
         if (packet_size == SOCKET_ERROR) {
             cout << "Can't send message to Server. Error # " << WSAGetLastError() << endl;
             closesocket(ClientSock);
             WSACleanup();
             return 1;
         }
+#endif
 
         packet_size = recv(ClientSock, servBuff.data(), servBuff.size(), 0);
 
+#ifdef _WIN32
         if (packet_size == SOCKET_ERROR) {
             cout << "Can't receive message from Server. Error # " << WSAGetLastError() << endl;
             closesocket(ClientSock);
@@ -131,9 +145,14 @@ int main(void)
         else
             cout << "Server message: " << servBuff.data() << endl;
     }
+#endif
 
+#ifdef _WIN32
     closesocket(ClientSock);
     WSACleanup();
+#else
+   close(ClientSock);
+#endif
 
     return 0;
 }
