0ece722aa5e5e5d7cb43c2fe5fff6b91966d2647
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-01-23 11:12:05 +0500

    Update eventloop.h

--- eventloop.h
+++ eventloop.h
@@ -1,20 +1,17 @@
 #pragma once
 #include <iostream>
-#include <thread>
 #include "icommand.h"
 #include "safequeue.h"
-#include "istate.h"
 
 class eventloop
 {
 public:
-    eventloop(SafeQueue<ICommand *> *cmds, StateStatus *status);
+    eventloop(SafeQueue<ICommand *> *cmds);
     ~eventloop();
 
     SafeQueue<ICommand *> *cmds;
-    StateStatus *status;
 
-    void start(SafeQueue<ICommand *> *cmds, StateStatus *status, int variant);
+    void start(SafeQueue<ICommand *> *cmds);
     void execute(ICommand *cmd);
 private:
     class eventloopP* imp;
