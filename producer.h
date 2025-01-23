fc8bfc1e96c937a65b224191a0735a7484a44f72
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-01-23 11:31:34 +0500

    Update producer.h

--- producer.h
+++ producer.h
@@ -5,8 +5,8 @@ class producer
 {
 public:
     void start_game();
-    void test_game(vector <char> message);
-    void read_message(vector <char> message);
+    void test_game(std::vector <char> message);
+    void read_message(std::vector <char> message);
 
     objectVector vector_obj;
     string gameId_s, objectId_s, operationId_s, args;
