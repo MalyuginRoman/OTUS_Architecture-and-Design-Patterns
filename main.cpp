4b5ce95176a65b39980a30465d7fec268b6c116a
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-03 23:22:38 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int ac, char **av)
 {
     std::cout << std::endl;
     std::cout << "Testing thread methods..." << std::endl;
-    //test_thread1();
+    test_thread1();
     //test_thread2();
     return 0;
 }
