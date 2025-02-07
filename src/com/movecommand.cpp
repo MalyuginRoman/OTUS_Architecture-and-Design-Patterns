0bf1db25c89e6bc2449ea9abb3e6cac066868c43
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-07 15:43:08 +0500

    Update movecommand.cpp

--- src/com/movecommand.cpp
+++ src/com/movecommand.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "icommand.h"
-#include "minor.h"
-#include "object.h"
+#include "../minor.h"
+#include "../object.h"
 const double eps = 10E-6;
 
 struct coord_interval
