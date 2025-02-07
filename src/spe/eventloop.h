3d7adc6885f6fb683846f3ef2e867551d2f0203c
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-07 15:56:38 +0500

    Update and rename src/eventloop.h to src/spe/eventloop.h

--- src/spe/eventloop.h
+++ src/spe/eventloop.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <iostream>
 #include <thread>
-#include "com/icommand.h"
-#include "sec/safequeue.h"
+#include "../com/icommand.h"
+#include "../sec/safequeue.h"
 #include "istate.h"
 
 class eventloop
