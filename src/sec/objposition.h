f7cf6f73de6def28ae0d3ac81d94345d88e75917
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-21 17:46:35 +0500

    Update objposition.h

--- src/sec/objposition.h
+++ src/sec/objposition.h
@@ -3,85 +3,67 @@
 #include <list>
 #include "object.h"
 
-struct coord_interval
+double local_ext(double f1, double f2, int num, int count)
 {
-public:
-  double Xmax;
-  double Xmin;
-  double Ymax;
-  double Ymin;
-};
-
-struct system_okr
-{
-public:
-  int id_syst_okr;
-  bool isEmpty;
-  int countObject;
-  coord_interval XY;
-  std::vector<int> obj_id;
-};
-
-struct objectCheck
-{
-public:
-  system_okr p_map;
-};
-
-std::map<int, system_okr> func_name(int number)          // формируем системы окрестностей
-{
-  double Xstart = 0.;
-  double Ystart = 0.;
-  int sizeXY = 0.;
-  std::map<int, system_okr> p_map_c_a;
-  int count_system;
-  if(number == 1)
-    count_system = count_system_1;
-  else
-    count_system = count_system_2;
-  for(int i = 0; i < count_system; i++)
-    for(int j = 0; j < count_system; j++)
-    {
-      system_okr a1;
-      a1.XY.Xmin = Xstart + i     * Xmax/count_system;
-      a1.XY.Xmax = Xstart +(i + 1)* Xmax/count_system;
-      a1.XY.Ymin = Xstart + j     * Ymax/count_system;
-      a1.XY.Ymax = Xstart +(j + 1)* Ymax/count_system;
-      a1.id_syst_okr = sizeXY;
-      a1.isEmpty = true;
-      a1.countObject = 0;
-      p_map_c_a.emplace(sizeXY, a1);
-      sizeXY ++;
-    } 
-  return p_map_c_a;
+    return (f1 + num * f2/count);
 }
 
-std::map<int, system_okr> func_obj(std::map<int, system_okr> p_map, objectVector *vector)    // помещаем объекты в системы окрестностей
+bool isXY(std::map<int, system_okr> p_map, objectVector *vector, int i, int j)
 {
-    int countObj = vector->count();
+    bool isX = ((p_map.at(j).XY.Xmin - vector->at(i)->place().placeX) < eps) &&
+               ((vector->at(i)->place().placeX - p_map.at(j).XY.Xmax) < eps);
+    bool isY = ((p_map.at(j).XY.Ymin - vector->at(i)->place().placeY) < eps) &&
+               ((vector->at(i)->place().placeY - p_map.at(j).XY.Ymax) < eps);
+    return (isX && isY);
+}
+
+std::map<int, system_okr> func_name(int number)                                             // формируем системы окрестностей
+{
+    double Xstart = 0.;
+    double Ystart = 0.;
+    int sizeXY = 0;
+    std::map<int, system_okr> p_map_c_a;
+    int count_system;
+    if(number == 1)
+        count_system = count_system_1;
+    else
+        count_system = count_system_2;
+
+    for(int i = 0; i < count_system; i++)
+        for(int j = 0; j < count_system; j++)
+        {
+            system_okr a1;
+            a1.XY.Xmin = local_ext(Xstart, Xmax, i, count_system);
+            a1.XY.Xmax = local_ext(Xstart, Xmax, i + 1, count_system);
+            a1.XY.Ymin = local_ext(Ystart, Ymax, j, count_system);
+            a1.XY.Ymax = local_ext(Ystart, Ymax, j + 1, count_system);
+            a1.id_syst_okr = sizeXY;
+            a1.isEmpty = true;
+            a1.countObject = 0;
+            p_map_c_a.emplace(sizeXY, a1);
+            sizeXY++;
+        }
+return p_map_c_a;
+}
+
+std::map<int, system_okr> func_obj(std::map<int, system_okr> p_map, objectVector *vector)   // помещаем объекты в системы окресностей
+{
+    size_t countObj = vector->count();
     std::map<int, system_okr> p_map_c = p_map;
     for(int i = 0; i < countObj; i++)
     {
-      double current_X = vector->at(i)->place().placeX;
-      double current_Y = vector->at(i)->place().placeY;
-      int count1 = p_map_c.size();
-      for(int j = 0; j < count1; ++j)
-      {
-        double x1 = p_map_c.at(j).XY.Xmin;
-        double x2 = p_map_c.at(j).XY.Xmax;
-        double y1 = p_map_c.at(j).XY.Ymin;
-        double y2 = p_map_c.at(j).XY.Ymax;
-        bool isX = ((x1 - current_X) < eps) && ((current_X - x2) < eps);
-        bool isY = ((y1 - current_Y) < eps) && ((current_Y - y2) < eps);
-        if(isX && isY)
+        size_t count1 = p_map_c.size();
+        for(int j = 0; j < count1; ++j)
         {
-          p_map_c.at(j).countObject++;
-          p_map_c.at(j).isEmpty = false;
-          int id_obj = vector->at(i)->id();
-          p_map_c.at(j).obj_id.push_back(id_obj);
-          break;
+            if(isXY(p_map_c, vector, i, j))
+            {
+                p_map_c.at(j).countObject++;
+                p_map_c.at(j).isEmpty = false;
+                int id_obj = vector->at(i)->objectID();
+                p_map_c.at(j).obj_id.push_back(id_obj);
+                break;
+            }
         }
-      }
     }
     return p_map_c;
 }
