335a916340d8ce3a78259966ffb3602872b2db73
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2025-02-21 17:01:36 +0500

    Update icommand.cpp

--- src/com/icommand.cpp
+++ src/com/icommand.cpp
@@ -1,86 +1,324 @@
+#ifndef ICOMMAND_H
+#define ICOMMAND_H
 #include <iostream>
-#include "icommand.h"
-using namespace std;
+#include <list>
+#include <map>
+#include <functional>
+#include "order.h"
 
-class MacroCommandP
+class objectVector;
+class object;
+struct system_okr;
+
+enum CommandCodes
+{
+    CommandMove = 1,
+    CommandRotate = 2,
+    CommandCheck = 3,
+    CommandBurn = 4,
+    CommandLoger = 5,
+    CommandEmpty = 6,
+    CommandHardStop = 7,
+    CommandSoftStop = 8,
+    CommandInternet = 9,
+    CommandMoveTo = 10,
+    CommandRun = 11,
+    CommandAddLast = 12,
+    CommandStart = 13,
+    CommandStop = 14,
+    CommandShoot = 15,
+    CommandMacro = 100
+};
+
+class ICommand
 {
 public:
-    vector<ICommand*> cmds;
+    virtual int get_Id_cmd() = 0;
+    virtual int get_Id_parent() = 0;
+    virtual void execute() = 0;
+};
 
-    MacroCommandP(vector<ICommand*> cmds) :
-        cmds(cmds)
+class MoveCommand : public ICommand
+{
+public:
+    MoveCommand(std::map<int, system_okr>* p_map_a,
+                std::map<int, system_okr>* p_map_b,
+                object* obj);
+    ~MoveCommand();
+
+    object* obj() const;
+    std::map<int, system_okr>* p_map_a() const;
+    std::map<int, system_okr>* p_map_b() const;
+
+    int get_Id_cmd();
+    int get_Id_parent();
+    void execute();
+
+private:
+    class MoveCommandP* imp;
+};
+
+class CheckPositionCommand
+{
+public:
+    void execute(std::map<int, system_okr>* p_map, object* obj);
+};
+
+class RotateCommand : public ICommand
+{
+public:
+    RotateCommand(object* obj);
+    ~RotateCommand();
+
+    object* obj() const;
+
+    int get_Id_cmd();
+    int get_Id_parent();
+    void execute();
+
+private:
+    class RotateCommandP* imp;
+};
+
+class CheckCommand : public ICommand
+{
+public:
+    int get_Id_cmd()
     {
+        return CommandCheck;
+    }
+    int get_Id_parent()
+    {
+        return -9999;
+    }
+    void execute()
+    {
+        std::cout << "CheckCommand" << std::endl;
     }
 };
 
-MacroCommand::MacroCommand(vector<ICommand*> cmds) :
-    imp(new MacroCommandP(cmds))
-{
-}
-
-MacroCommand::~MacroCommand() { delete imp;}
-
-void MacroCommand::execute()
-{
-    cout << "Start execute of MacroCommand" << endl;
-    if(imp->cmds.empty())
-        throw runtime_error ("Сommand list is empty");
-    for(ICommand* i : imp->cmds)
-    {
-        try
-        {
-            i->execute();
-        } catch (...) {
-            throw runtime_error ("Pass the error up");
-        }
-    }
-}
-class RegisterCommandP
+class BurnCommand : public ICommand
 {
 public:
-    map<string, function<ICommand*()>> *m_map;
-    map<string, string> *m_scope;
-
-    RegisterCommandP(map<string, function<ICommand*()>> *m_map, map<string, string> *m_scope) :
-        m_map(m_map),
-        m_scope(m_scope)
+    int get_Id_cmd()
     {
+        return CommandBurn;
+    }
+    int get_Id_parent()
+    {
+        return -9999;
+    }
+    void execute()
+    {
+        std::cout << "BurnCommand" << std::endl;
     }
 };
 
-RegisterCommand::RegisterCommand(map<string, function<ICommand*()>> *m_map, map<string, string> *m_scope) :
-    imp(new RegisterCommandP(m_map, m_scope))
-{
-}
-
-RegisterCommand::~RegisterCommand() { delete imp;}
-
-void RegisterCommand::execute()
-{
-}
-
-void RegisterCommand::registerType(string key_s, string key_f, function<ICommand*()> func)
-{
-    imp->m_scope->emplace(key_s, key_f);
-    imp->m_map->emplace(key_f, func);
-    cout << "Registre " << key_f << " in " << key_s << endl;
-}
-
-class CheckPositionCommandP
+class LogerCommand : public ICommand
 {
 public:
-    std::map<int, std::list<objectVector>> *p_map_o_a;
-    std::map<int, std::pair<double, double>> *p_map_c_a;
-    std::map<int, std::list<objectVector>> *p_map_o_b;
-    std::map<int, std::pair<double, double>> *p_map_c_b;
-
-    CheckPositionCommandP(std::map<int, std::list<objectVector>> *p_map_o_a,
-                          std::map<int, std::pair<double, double>> *p_map_c_a,
-                          std::map<int, std::list<objectVector>> *p_map_o_b,
-                          std::map<int, std::pair<double, double>> *p_map_c_b) :
-            p_map_o_a(p_map_o_a),
-            p_map_c_a(p_map_c_a),
-            p_map_o_b(p_map_o_b),
-            p_map_c_b(p_map_c_b)
-    {}
+    int get_Id_cmd()
+    {
+        return CommandLoger;
+    }
+    int get_Id_parent()
+    {
+        return -9999;
+    }
+    void execute()
+    {
+        std::cout << "LogerCommand" << std::endl;
+    }
 };
+
+class EmptyCommand : public ICommand
+{
+public:
+    int get_Id_cmd()
+    {
+        return CommandEmpty;
+    }
+    int get_Id_parent()
+    {
+        return -9999;
+    }
+    void execute()
+    {
+        std::cout << "EmptyCommand" << std::endl;
+    }
+};
+
+class HardStopCommand : public ICommand
+{
+public:
+    int get_Id_cmd()
+    {
+        return CommandHardStop;
+    }
+    int get_Id_parent()
+    {
+        return -9999;
+    }
+    void execute()
+    {
+        std::cout << "HardStopCommand" << std::endl;
+    }
+};
+
+class SoftStopCommand : public ICommand
+{
+public:
+    int get_Id_cmd()
+    {
+        return CommandSoftStop;
+    }
+    int get_Id_parent()
+    {
+        return -9999;
+    }
+    void execute()
+    {
+        std::cout << "SoftStopCommand" << std::endl;
+    }
+};
+
+class InternetCommand : public ICommand
+{
+public:
+    InternetCommand(objectVector* vect, object* obj, order *order_cmd, std::list<ICommand*> cmds);
+    ~InternetCommand();
+
+    objectVector* vect;
+    object* obj;
+    order *order_cmd;
+    std::list<ICommand*> cmds;
+
+    int get_Id_cmd()
+    {
+        return CommandInternet;
+    }
+    void execute();
+    int get_Id_parent();
+
+    bool create();
+private:
+    class InternetCommandP* imp;
+};
+
+class MoveToCommand : public ICommand
+{
+public:
+    int get_Id_cmd()
+    {
+        return CommandMoveTo;
+    }
+    int get_Id_parent()
+    {
+        return -9999;
+    }
+    void execute()
+    {
+        std::cout << "MoveToCommand" << std::endl;
+    }
+};
+
+class RunCommand : public ICommand
+{
+public:
+    int get_Id_cmd()
+    {
+        return CommandRun;
+    }
+    int get_Id_parent()
+    {
+        return -9999;
+    }
+    void execute()
+    {
+        std::cout << "RunCommand" << std::endl;
+    }
+};
+
+class AddLastCommand : public ICommand
+{
+public:
+    int get_Id_cmd()
+    {
+        return CommandAddLast;
+    }
+    int get_Id_parent()
+    {
+        return -9999;
+    }
+    void execute()
+    {
+        std::cout << "AddLastCommand" << std::endl;
+    }
+};
+
+class StartMotion : public ICommand
+{
+public:
+    StartMotion(object* obj, double du);
+    ~StartMotion();
+
+    object* obj() const;
+    double du() const;
+
+    int get_Id_cmd();
+    int get_Id_parent();
+    void execute();
+
+private:
+    class StartMotionP* imp;
+};
+
+class StopMotion : public ICommand
+{
+public:
+    StopMotion(object* obj);
+    ~StopMotion();
+
+    object* obj() const;
+
+    int get_Id_cmd();
+    int get_Id_parent();
+    void execute();
+
+private:
+    class StopMotionP* imp;
+};
+
+class ShootCommand : public ICommand
+{
+public:
+    ShootCommand(objectVector* vect, object* obj);
+    ~ShootCommand();
+
+    objectVector* vect() const;
+    object* obj() const;
+
+    int get_Id_cmd();
+    int get_Id_parent();
+    void execute();
+
+private:
+    class ShootCommandP* imp;
+};
+
+class MacroCommand : public ICommand
+{
+public:
+    int get_Id_cmd()
+    {
+        return CommandMacro;
+    }
+    int get_Id_parent();
+    MacroCommand(std::list<ICommand*> cmds);
+    ~MacroCommand();
+    std::list<ICommand*> cmds;
+    void execute();
+private:
+    class MacroCommandP* imp;
+};
+#endif // ICOMMAND_H
